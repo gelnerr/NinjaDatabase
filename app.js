a5100ac6d8a02f595ff5fc487306e74bc261edb4
gelnerr <glenissac392@gmail.com>
2026-04-04 11:59:23 -0600

    Node initialization, ejs conversion

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Static files middleware
+app.use(express.static(path.join(__dirname, 'public')));
+// Serve bootstrap and bootstrap-icons from node_modules
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/icons', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
